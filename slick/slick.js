b6a278c3c4a57bc4fa29d905203248ef8963e629
hrufon <33417744+hrufon@users.noreply.github.com>
2020-01-16 21:10:14 +0200

    Update slick.js

--- slick/slick.js
+++ slick/slick.js
@@ -9,4 +9,14 @@ $(document).ready(function(){
         prevArrow: $('.prev'),
         nextArrow: $('.next'),
     });
+    $('.slick_slider_two').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        cssEase: 'linear',
+        prevArrow: $('.prev_happy_clients'),
+        nextArrow: $('.next_happy_clients'),
 });
+})
